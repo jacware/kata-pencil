2c8094994880a559420836ae3de507d49dcfc9c7
= <aaron@jacware.com>
2017-10-29 08:18:57 -0400

    tests for edge cases

--- models/paper.rb
+++ models/paper.rb
@@ -20,7 +20,11 @@ class Paper
   end
 
   def edit substr, index
+    #check if index is in the bounds of the string
+    return if index < 0 or index > (@text.length - index)
     substr.split("").each_with_index do |char,i|
+      #get the location of the current replacement char
+      #and determine if we are editing a blank space or overwriting
       pos = index + i
       if @text[pos] == ' '
         @text[pos] = char
--- tests/test_paper.rb
+++ tests/test_paper.rb
@@ -15,6 +15,12 @@ class TestPaper < Minitest::Test
     assert_equal "     1     ", paper.text
   end
 
+  def test_ereasing_non_existant_string
+    paper = Paper.new "test 1 test"
+    paper.erase "abc"
+    assert_equal "test 1 test", paper.text
+  end
+
   def test_editing
     paper = Paper.new "An       a day keeps the doctor away"
     paper.edit "onion", 3
@@ -25,5 +31,19 @@ class TestPaper < Minitest::Test
     paper = Paper.new "An       a day keeps the doctor away"
     paper.edit "artichoke", 3
     assert_equal "An artich@k@ay keeps the doctor away", paper.text
+  end
+
+  def test_editing_out_of_bounds
+    paper = Paper.new "a        a"
+    paper.edit "test", -1
+    assert_equal "a        a", paper.text
+    paper.edit "test", 100
+    assert_equal "a        a", paper.text
+  end
+
+  def test_editing_string_longer_then_paper
+    paper = Paper.new "a   a"
+    paper.edit "testtesttest", 1
+    assert_equal "ates@@@@@@@@@", paper.text
   end
 end
