662acc01f98ef45aa5bbbdc4b85e8f3ee85747f5
= <aaron@jacware.com>
2017-10-29 05:34:33 -0400

    remove unused functions

--- ext/string.rb
+++ ext/string.rb
@@ -1,20 +1,8 @@
 class String
-  def lowercase?
-    return false if self.size > 1
-    ('a'..'z').include? self
-  end
 
   def uppercase?
     return false if self.size > 1
     ('A'..'Z').include? self
-  end
-
-  def lowercase_count
-    count = 0
-    self.split("").each do |char|
-      count += 1 if char.lowercase?
-    end
-    count
   end
 
   def uppercase_count
@@ -27,5 +15,13 @@ class String
 
   def printable_length
     self.gsub(/ |\n/, '').length
+  end
+
+  def to_integer_or_nil
+    if self == '' or self.nil? or self.to_i.to_s != self
+      return nil
+    else
+      return self.to_i
+    end
   end
 end
--- tests/test_ext_string.rb
+++ tests/test_ext_string.rb
@@ -2,28 +2,26 @@ require "minitest/autorun"
 require_relative "../config.rb"
 
 class TestExtString < Minitest::Test
-  def test_lower
-    assert "a".lowercase?
-  end
-
   def test_upper
     assert "A".uppercase?
-  end
-
-  def test_lower_count
-    assert_equal 2, "AAaaBB".lowercase_count
+    assert !"a".uppercase?
   end
 
   def test_upper_count
     assert_equal 4, "AAaaBB".uppercase_count
   end
 
-  def test_numbers
+  def test_upper_count_with_numbers
     assert_equal 2, "AAbbb12".uppercase_count
-    assert_equal 3, "AAbbb12".lowercase_count
   end
 
-  def test_count_non_whitespace
+  def test_printable_length
     assert_equal 4, "11 1\n1".printable_length
+  end
+
+  def test_to_integer_or_nil
+    assert_equal 3, "3".to_integer_or_nil
+    assert_equal nil, "".to_integer_or_nil
+    assert_equal nil, "abc1".to_integer_or_nil
   end
 end
