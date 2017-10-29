f95f435a69a8b06a8448cab4a90708e7aa04f7bd
= <aaron@jacware.com>
2017-10-28 20:01:00 -0400

    added pencil durability

--- models/pencil.rb
+++ models/pencil.rb
@@ -1,5 +1,12 @@
 class Pencil
+  attr_reader :durability
+
+  def initialize params={}
+    @durability = params[:durability] || 1000
+  end
+
   def write text, paper
+    @durability -= text.length
     paper.append text
   end
 end
--- tests/test_pencil.rb
+++ tests/test_pencil.rb
@@ -2,12 +2,20 @@ require "minitest/autorun"
 require_relative "../config.rb"
 
 class TestPencil < Minitest::Test
+  def setup
+    @paper = Paper.new
+  end
   def test_writing_on_paper
-    paper = Paper.new
     pencil = Pencil.new
-    pencil.write "test", paper
-    assert_equal "test", paper.text
-    pencil.write " 2", paper
-    assert_equal "test 2", paper.text
+    pencil.write "test", @paper
+    assert_equal "test", @paper.text
+    pencil.write " 2", @paper
+    assert_equal "test 2", @paper.text
+  end
+
+  def test_pencil_durability
+    pencil = Pencil.new durability: 100
+    pencil.write "test", @paper
+    assert_equal pencil.durability, 96
   end
 end
