b89348943cdb1a520fc33dab2220d147bb02a38f
= <aaron@jacware.com>
2017-10-30 01:59:11 -0400

    refactor eraser durability to follow same convention as pencil

--- models/pencil.rb
+++ models/pencil.rb
@@ -12,26 +12,18 @@ class Pencil
     paper.append text_after_durability_check(text)
   end
 
-  def sharpen
-    @length -= 1 if @length
-    return if @length and @length < 0
-    @durability = @original_durability
-  end
-
-  def erase string, paper
-    if @eraser_durability
-      @eraser_durability -= string.printable_length
-      #if the durability is less then 0 then only erase the amount available
-      if @eraser_durability < 0
-        string = string[@eraser_durability.abs..string.length]
-        @eraser_durability = 0
-      end
-    end
-    paper.erase string
+  def erase text, paper
+    paper.erase text_after_eraser_durability_check(text)
   end
 
   def edit text, index, paper
     paper.edit text_after_durability_check(text), index
+  end
+
+  def sharpen
+    @length -= 1 if @length
+    return if @length and @length < 0
+    @durability = @original_durability
   end
 
 private
@@ -41,6 +33,17 @@ private
       if @durability < 0
         @durability.abs.times {|i| text[text.length-i-1] = ' '}
         @durability = 0
+      end
+    end
+    text
+  end
+
+  def text_after_eraser_durability_check text
+    if @eraser_durability
+      @eraser_durability -= text.printable_length
+      if @eraser_durability < 0
+        text = text[@eraser_durability.abs..text.length]
+        @eraser_durability = 0
       end
     end
     text
